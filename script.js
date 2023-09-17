424e4be87d8ba01c4b1b7271a508ad8fce6b6e91
mhaemnn <muhaemin1998@gmail.com>
2023-09-17 21:57:19 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-const parag = document.createElement("li");
-parag.innerText = "ini adalah paragraf menggunkan javascript";
+/*
+macam-macam event
+1. event window
+    a. onload
+    b. onpageshow
+    c. onriseze
+2. event from
+    a. onreset
+    b. onsubmit
+3. event keyboard
+    a. onkeydown
+    b. onkeypress
+    c. onkeyup 
+4. clipboard event
+    a. oncopy
+    b. oncut
+    c. onpaste
+5. mouse event
+    a. onclik
+    b. ondoublecklick
+    c. onmouseover
 
-const datar = document.getElementById("daftar");
-datar.appendChild(parag);
+ */
 
-/////
-const elementAwal = document.createElement("li");
-elementAwal.innerText = "hidupkan kompor bro";
+function increment() {
+  document.getElementById("count").innerText++;
 
-const itemAwal = document.getElementById("awal");
-datar.insertBefore(elementAwal, itemAwal);
+  if (document.getElementById("count").innerText == 7) {
+    const hiddenMessage = document.createElement("h4");
+    hiddenMessage.innerText = "Selamat! Anda menemukan hadiah tersembunyi...";
+    const image = document.createElement("img");
+    image.setAttribute("src", "https://i.ibb.co/0V49VRZ/catto.jpg");
+    image.style.margin = "auto";
+    const contents = document.querySelector(".contents");
+    contents.appendChild(hiddenMessage).appendChild(image);
+  }
+}
+
+function welcome() {
+  alert("Sim salabim muncullah elemen-elemen HTML!");
+  const contents = document.querySelector(".contents");
+  contents.removeAttribute("hidden");
+}
+
+document.getElementById("incrementButton").onclick = increment;
+document.body.onload = welcome;
